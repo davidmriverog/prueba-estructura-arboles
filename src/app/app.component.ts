bd2817c0ed8e27890af4427f18eee0e5e95f7161
David Rivero <davidmriverog@gmail.com>
2019-05-07 05:01:01 -0300

    resulto primera pregunta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,23 +41,61 @@ export class AppComponent implements OnInit {
 				[13]
 		  ];
 
-		  this.treeList.forEach((item, index) => {
+		  this.buildNodeList();
 
-		  	let nodeObj : NodeData = {
-		  		key: item[0],
-		  		childLeft: item.length > 1 ? item[1] : null,
-		  		rightRight: item.length === 3 ? item[2] : null
-		  	};
+		  // 1) Dado un número dado, encontrar sus ancestros. (3, 5, 8, 12)
+		  this.findAncestors(3);
+		  this.findAncestors(5);
+		  this.findAncestors(8);
+		  this.findAncestors(12);
 
-		  	this.nodeList.push(nodeObj);
-		  })
+		  // 2) Dado un número dado, encontrar sus hijos. (1, 6, 9, 11)
+		  
 
-		  console.log('node.list', this.nodeList);
-  		
   	}catch(e) {
 
   		console.log('exception.handle', e);
   	}
+  }
+
+  buildNodeList() : void {
+  	this.treeList.forEach((item, index) => {
+
+	  	let nodeObj : NodeData = {
+	  		key: item[0],
+	  		childLeft: item.length > 1 ? item[1] : null,
+	  		rightRight: item.length === 3 ? item[2] : null
+	  	};
+
+	  	this.nodeList.push(nodeObj);
+	  })
+
+	  console.log('node.list', this.nodeList);
+
+  }
+
+  findAncestors(key : number) :NodeData[] {
+
+  	console.log('INIT - FIND ANCESTORS['+key+']');
+
+  	let findAncestors : NodeData[] = this.nodeList.filter((item, index) => {
+
+  		return item.childLeft === key || item.rightRight === key;
+  	});
+
+  	if (!findAncestors.length) {
+  		console.log('he '+key+' does not have ancestors');
+  	} else {
+  		console.log('the Ancestors for ' + key + 'is: ', findAncestors);	
+  	}
+
+  	console.log('FINISHED - FIND ANCESTORS['+key+']');
+
+  	return findAncestors;
+  }
+
+  findChildByKeyNode(key : number) : void {
+
   }
 
 }
